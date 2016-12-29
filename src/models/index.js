fc9c010201b68e6d17ab2e5c2bf0bf38b05b905c
Eric Buchmann <me@ebuchmann.com>
2016-12-28 19:09:17 -0800

    fixed import logic to not include map files

--- src/models/index.js
+++ src/models/index.js
@@ -2,7 +2,12 @@ import fs from 'fs';
 import path from 'path';
 
 const result = {};
-const models = fs.readdirSync(__dirname).filter((file) => file !== path.basename(module.filename));
+const models = fs.readdirSync(__dirname).filter((file) => 
+  (file.indexOf('.') !== 0) && // not hidden
+  (file !== path.basename(module.filename)) && // not this file
+  (file !== 'test.js') && // not test.js
+  (path.extname(file) === '.js') // is a .js file, not .map file
+);
 
 models.forEach((model) => {
   result[model.substr(0, model.length - 3)] = require(path.join(__dirname, model));
